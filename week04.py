6330e97d6fed48d000881505a8f648de5baec8c0
deepblue <deepblue@inha.ac.kr>
2026-03-24 17:58:36 +0900

    week04 dataframe xi

--- week04.py
+++ week04.py
@@ -1,10 +1,9 @@
 import pandas as pd
+import seaborn as sns
 
-items = {
-    'a': [100, 80, 90],
-    'b': [95, 75, 85],
-    'c': [70, 100, 99]
-}
-df_items = pd.DataFrame(items, index=[1, 2, 3])
-print(df_items)
-print(df_items.apply(lambda n : n * n)) # Apply function to each object.
+mpg = sns.load_dataset('mpg')
+print(mpg.info())
+# mpg.dropna(subset=['horsepower'], inplace=True)  # 원본 업데이트
+# print(mpg.info())
+mpg_clean = mpg.dropna(subset=['horsepower'])  # 사본 생성
+print(mpg_clean.info())
