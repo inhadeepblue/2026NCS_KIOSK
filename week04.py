ca039038f91bc7e43f6ed3bc18aeef590f63e149
deepblue <deepblue@inha.ac.kr>
2026-03-24 17:35:43 +0900

    week04 dataframe ix

--- week04.py
+++ week04.py
@@ -1,9 +1,13 @@
-import seaborn as sns
 import pandas as pd
 
-mpg = sns.load_dataset('mpg')
-# print(mpg.groupby(by='origin'))
-group_mpg = pd.DataFrame(mpg.groupby(by='origin'))
-print(group_mpg)
-print(mpg['origin'].nunique())
-print(mpg.describe())
+def squares(n):
+    return n * n
+
+items = {
+    'a': [100, 80, 90],
+    'b': [95, 75, 85],
+    'c': [70, 100, 99]
+}
+df_items = pd.DataFrame(items, index=[1, 2, 3])
+print(df_items)
+print(df_items.apply(squares)) # Apply function to each object.
