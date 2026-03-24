684d6945288ab4ea35319f24975fe193e4556e6d
deepblue <deepblue@inha.ac.kr>
2026-03-24 17:36:46 +0900

    week04 dataframe x

--- week04.py
+++ week04.py
@@ -1,8 +1,5 @@
 import pandas as pd
 
-def squares(n):
-    return n * n
-
 items = {
     'a': [100, 80, 90],
     'b': [95, 75, 85],
@@ -10,4 +7,4 @@ items = {
 }
 df_items = pd.DataFrame(items, index=[1, 2, 3])
 print(df_items)
-print(df_items.apply(squares)) # Apply function to each object.
+print(df_items.apply(lambda n : n * n)) # Apply function to each object.
