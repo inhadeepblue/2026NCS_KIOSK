b6c1c8cbf671ab161e3b8bf20d73c94e00a78fb3
deepblue <deepblue@inha.ac.kr>
2026-04-28 19:02:39 +0900

    week09 kiosk v1.2 (db)

--- week09.py
+++ week09.py
@@ -1,8 +1,11 @@
 import kiosk
 
 if __name__ == "__main__":
-    menu_drinks = ["Ice Americano", "Cafe Latte", "Watermelon Juice", "Espresso"]
-    menu_prices = [2000, 3000, 4900, 2500]
+    # menu_drinks = ["Ice Americano", "Cafe Latte", "Watermelon Juice", "Espresso"]
+    # menu_prices = [2000, 3000, 4900, 2500]
+
+    menu_drinks = ["Ice Americano", "Cafe Latte"]
+    menu_prices = [2000, 3000]
 
     menu = kiosk.Menu(menu_drinks, menu_prices)
     order_processor = kiosk.OrderProcessor(menu)
